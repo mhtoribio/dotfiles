604093acb9ff37bf85cb74d32a660dd5c5e90da8
Markus Heinrich Toribio <markusht1@gmail.com>
2022-12-19 11:46:35 +0100

    try to unmap <CR> for accept lsp suggestion

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,3 +2,17 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 lsp.setup()
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+-- disable completion with tab
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+cmp_mappings['<CR>'] = nil
