56922258de294362b98cb87ca12017288660f9e7
Markus Heinrich Toribio <markusht1@gmail.com>
2023-01-15 22:32:10 +0100

    add <leader><leader><CR> keymap for executing current line as shell command

--- lua/mhtoribio/remap.lua
+++ lua/mhtoribio/remap.lua
@@ -22,3 +22,4 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader><leader><CR>", "<cmd>.!sh<CR>")
