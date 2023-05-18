312abdec9e3f6ace47a742eb1fb1d6db6ec1573f
Markus Heinrich Toribio <23717111+mhtoribio@users.noreply.github.com>
2023-05-18 05:38:23 -0400

    symlink xprofile when installing dotfiles

--- install/install_dotfiles.sh
+++ install/install_dotfiles.sh
@@ -31,6 +31,8 @@ ln -sv $HOME/.config/zsh/.zshenv $HOME/.zshenv
 ln -sv $HOME/.dotfiles/i3 $HOME/.config/i3
 ln -sv $HOME/.dotfiles/i3status $HOME/.config/i3status
 ln -sv $HOME/.dotfiles/alacritty/ $HOME/.config/alacritty
+ln -sv $HOME/.dotfiles/x11/.xprofile ~/.xsessionrc
+ln -sv $HOME/.dotfiles/x11/.xprofile ~/.xprofile
 
 # Neovim
 git clone git@github.com:mhtoribio/nvim.conf $HOME/.config/nvim
