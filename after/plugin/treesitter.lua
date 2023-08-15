147abe652bfe43a2c0f353ca9a7dd32fd71b7204
Markus Heinrich Toribio <23717111+mhtoribio@users.noreply.github.com>
2023-08-15 15:24:08 +0200

    update language "help" to "vimdoc" so treesitter does not error on startup

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all"
-	ensure_installed = { "help", "c", "lua", "cpp", "latex", "rust" },
+	ensure_installed = { "vimdoc", "c", "lua", "cpp", "latex", "rust" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
