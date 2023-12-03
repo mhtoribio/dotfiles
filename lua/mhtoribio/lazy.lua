0c61d45046f03a5784193e88554d51e810c8ac77
Markus Heinrich Toribio <23717111+mhtoribio@users.noreply.github.com>
2023-12-03 14:44:38 +0100

    migrate to lazy.nvim

---
+++ lua/mhtoribio/lazy.lua
@@ -0,0 +1,67 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = {'nvim-lua/plenary.nvim'},
+        config = function() require("mhtoribio.plugins.telescope") end,
+    },
+
+    {"ellisonleao/gruvbox.nvim", config = function() require("mhtoribio.plugins.colors") end},
+
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = function() vim.fn["TSUpdate"]() end,
+        config = function() require("mhtoribio.plugins.treesitter") end
+    },
+    {'nvim-treesitter/playground'},
+    {
+        'theprimeagen/harpoon',
+        lazy = false,
+        dependencies = {"nvim-lua/plenary.nvim"},
+        config = function() require("mhtoribio.plugins.harpoon") end,
+    },
+    {'tpope/vim-fugitive', config = function() require("mhtoribio.plugins.fugitive") end},
+
+    -- LSP Support
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch='v3.x',
+        dependencies = {
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+        },
+        config = function() require("mhtoribio.plugins.lsp") end,
+    },
+
+    {'junegunn/vim-easy-align', config = function() require("mhtoribio.plugins.align") end},
+
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    }
+}
+
+require("lazy").setup(plugins, opts)
--- lua/mhtoribio/plugins/lsp.lua
+++ lua/mhtoribio/plugins/lsp.lua
@@ -4,12 +4,6 @@ lsp.preset('recommended')
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-})
 
 cmp.setup({
     completion = {
@@ -17,11 +11,13 @@ cmp.setup({
     },
     sources = {
         { name = 'orgmode' }
-    }
-})
-
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+    }),
 })
 
 lsp.on_attach(function(client, bufnr)
