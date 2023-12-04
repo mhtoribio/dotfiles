019ae2027229b56021065bd98d9bdaae584213f4
Markus Heinrich Toribio <23717111+mhtoribio@users.noreply.github.com>
2023-12-04 21:30:36 +0100

    auto-configure mason language servers

--- lua/mhtoribio/plugins/lsp.lua
+++ lua/mhtoribio/plugins/lsp.lua
@@ -58,6 +58,11 @@ return {
                     "rust_analyzer",
                 },
             }
+            require("mason-lspconfig").setup_handlers {
+                function (server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {}
+                end,
+            }
         end
     },
 }
