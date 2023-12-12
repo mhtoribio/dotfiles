f82f24acecfae3adfc169d63f556f979d96788a6
Markus Heinrich Toribio <23717111+mhtoribio@users.noreply.github.com>
2023-12-12 18:33:06 +0100

    refine keybindings for lsp

--- lua/mhtoribio/plugins/lsp.lua
+++ lua/mhtoribio/plugins/lsp.lua
@@ -10,7 +10,11 @@ return {
                 desc = 'LSP actions',
                 callback = function(event)
                     local opts = {buffer = event.buf}
-                    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                    vim.keymap.set('n', '<leader>vgd', function() vim.lsp.buf.definition() end, opts)
+                    vim.keymap.set('n', '<leader>vgD', function() vim.lsp.buf.declaration() end, opts)
+                    vim.keymap.set('n', '<leader>vgi', function() vim.lsp.buf.implementation() end, opts)
+                    vim.keymap.set('n', '<leader>vgt', function() vim.lsp.buf.type_definition() end, opts)
+                    vim.keymap.set('n', '<leader>vgs', function() vim.lsp.buf.signature_help() end, opts)
                     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
                     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
                     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
