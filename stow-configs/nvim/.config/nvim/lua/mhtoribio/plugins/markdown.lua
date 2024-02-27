5f6c93717c182b313dee01d4c820df91799278ba
Markus Heinrich Toribio <23717111+mhtoribio@users.noreply.github.com>
2024-02-27 11:50:14 +0100

    add markdown-image-paste

--- stow-configs/nvim/.config/nvim/lua/mhtoribio/plugins/markdown.lua
+++ stow-configs/nvim/.config/nvim/lua/mhtoribio/plugins/markdown.lua
@@ -1,6 +1,20 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+    {
+        'mhtoribio/markdown-image-paste',
+        -- 'fred441a/markdown-image-paste',
+        -- dir = '~/projects/markdown-image-paste',
+        ft = "markdown",
+        config = function() 
+            require('markdown-image-paste').setup({
+                custom_filename = true,
+            })
+            vim.api.nvim_set_keymap('n','<C-i>', "<cmd>lua require('markdown-image-paste').pasteImage()<CR>", {noremap = true})
+        end
+    }
 }
