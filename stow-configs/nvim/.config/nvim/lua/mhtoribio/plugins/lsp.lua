164aa81010942cb650925813545912d969ee0671
Markus Heinrich Toribio <23717111+mhtoribio@users.noreply.github.com>
2024-03-06 17:27:37 +0100

    add plugin for auto formatting rust code on save

--- stow-configs/nvim/.config/nvim/lua/mhtoribio/plugins/lsp.lua
+++ stow-configs/nvim/.config/nvim/lua/mhtoribio/plugins/lsp.lua
@@ -69,4 +69,11 @@ return {
             }
         end
     },
+    {
+        "rust-lang/rust.vim",
+        ft = "rust",
+        init = function()
+            vim.g.rustfmt_autosave = 1
+        end
+    },
 }
